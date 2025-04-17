b05a7aeab19d0a621ef8db7d7ea6a635953746d1
Pascal GAULT <pascal@inrage.fr>
2025-04-17 15:44:09 +0200

    feat: add PHP 8.4 support

--- stability-tags/docker-wordpress.sh
+++ stability-tags/docker-wordpress.sh
@@ -4,4 +4,4 @@ set -e
 
 . ../update.sh
 
-update_and_rebuild "inrage/docker-wordpress" "inrage/docker-php" "7.4 8.0 8.1 8.2 8.3"
+update_and_rebuild "inrage/docker-wordpress" "inrage/docker-php" "7.4 8.0 8.1 8.2 8.3 8.4"
