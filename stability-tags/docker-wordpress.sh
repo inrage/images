5b4911256df2d30a7d62e1fd243e6c69d0e9a669
Pascal GAULT <pascal@inrage.fr>
2025-07-09 09:59:00 +0200

    chore: remove support for PHP 7.4 and PHP 8.0

--- stability-tags/docker-wordpress.sh
+++ stability-tags/docker-wordpress.sh
@@ -4,4 +4,4 @@ set -e
 
 . ../update.sh
 
-update_and_rebuild "inrage/docker-wordpress" "inrage/docker-php" "7.4 8.0 8.1 8.2 8.3 8.4"
+update_and_rebuild "inrage/docker-wordpress" "inrage/docker-php" "8.1 8.2 8.3 8.4"
